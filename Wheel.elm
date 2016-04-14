f8cade2ccd3f7ee08e0c8e026b9d14ca29d2a079
Zain Hoda <zain@alphahat.com>
2016-04-14 00:06:20 -0400

    Add some more wiring

--- Wheel.elm
+++ Wheel.elm
@@ -2,19 +2,50 @@ import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Time exposing (..)
+import Mouse
 
+type Action = MousePosition (Int, Int)
+            | MouseDown
+            | MouseUp
+            | NewTime Float
+
+type alias Model = { t: Float
+                   , velocity: Float
+                   , lastMousePosition: (Int, Int)
+                   , mouseIsDown: Bool
+                    }
+
+init : Model
+init =
+  Model 0 20 (0, 0) False
 
 main : Signal Element
 main =
-  Signal.map clock (every millisecond)
+  Signal.map view (Signal.foldp update init signal)
 
+signal : Signal Action
+signal =
+  (Signal.mergeMany
+    [Signal.map MousePosition Mouse.position
+    ,Signal.map (\upDown -> if upDown then MouseDown else MouseUp) Mouse.isDown
+    ,Signal.map NewTime (every millisecond)
+    ]
+    )
 
-clock : Float -> Element
-clock t =
+update : Action -> Model -> Model
+update action model =
+  case action of
+    MousePosition pos -> {model | lastMousePosition = pos}
+    MouseDown -> {model | mouseIsDown = True, velocity = model.velocity * 2}
+    MouseUp -> {model | mouseIsDown = False}
+    NewTime t -> {model | t = t}
+
+view : Model -> Element
+view model =
   collage 400 400
     [ filled lightGrey (circle 110)
     , outlined (solid grey) (circle 110)
-    , hand orange 100 t 20
+    , hand orange 100 model.t model.velocity
     ]
 
 
